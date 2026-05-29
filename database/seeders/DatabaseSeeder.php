1bd5ee7c19e29ddccc5790f2dae1c8b1db9fe30d
devrf123 <devrefun@gmail.com>
2026-05-29 22:26:56 +0700

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+// use Illuminate\Database\Seeder;
 
 use App\Models\Setting;
 use App\Models\User;
